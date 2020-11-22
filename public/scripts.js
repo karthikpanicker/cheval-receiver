cb0432badf8915d39e6f6cc0430130681fdf7073
Karthik Raveendran <karthik.panicker@gmail.com>
2020-11-22 17:34:52 +0530

    Connect to local connect server

--- public/scripts.js
+++ public/scripts.js
@@ -26,7 +26,7 @@ function connect(roomId) {
         playEvent = new Event('smartRemote:PLAY'),
         pauseEvent = new Event('smartRemote:PAUSE')
     if (window["WebSocket"]) {
-        var conn = new WebSocket( 'ws://10.0.0.7:9001/ws'+ '?room-id=' + roomId);
+        var conn = new WebSocket( 'ws://10.0.0.4:9001/ws'+ '?room-id=' + roomId);
         conn.onclose = function (evt) {
             console.log('connection has been closed. Reconnect will be attempted in 1 second.', evt.reason);
             window._setTimeout(function() {
